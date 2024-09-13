c05317c92f844fb6d8c0e32d8dec4c3277afe173
jedcua <jonedwardcua@gmail.com>
2024-09-13 22:53:36 +0800

    Add test for build_context()

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ mod lib {
 use crate::datasource::ds::DataSource;
 use crate::datasource::ds::DataSource::{AppDescription, CloudwatchLogInsight, CloudwatchMetric, Ec2, Rds};
 use crate::datasource::ec2;
+use crate::lib::args::Args;
 use crate::lib::config::Config;
 use crate::lib::openai::OpenAiChatInput;
 use crate::lib::{args, openai, prompt};
@@ -56,7 +57,11 @@ struct AppContext {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let context = build_context().await?;
+    let args = args::Args::parse();
+    let toml_content = fs::read_to_string(&args.file).await?;
+    let config: Config = toml::from_str(&toml_content)?;
+
+    let context = build_context(args, config)?;
 
     let banner = BANNER.replace("{version}", env!("CARGO_PKG_VERSION"));
     println!("{banner}");
@@ -79,11 +84,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn build_context() -> Result<AppContext, Box<dyn Error>> {
-    let args = args::Args::parse();
-    let toml_content = fs::read_to_string(&args.file).await?;
-    let config: Config = toml::from_str(&toml_content)?;
-
+fn build_context(args: Args, config: Config) -> Result<AppContext, Box<dyn Error>> {
     let time_zone = match config.general.time_zone {
         Some(tz) => tz.parse().expect("Unknown time zone"),
         None => Tz::UTC
@@ -154,4 +155,89 @@ async fn build_context() -> Result<AppContext, Box<dyn Error>> {
     };
 
     Ok(context)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::lib::config::{AppDescConfig, CloudwatchLogInsightConfig, CloudwatchMetricConfig, Ec2Config, GeneralConfig, OpenAiConfig, RdsConfig};
+    use std::matches;
+
+    #[test]
+    fn build_context_without_errors() {
+        let context = build_context(
+            Args {
+                file: String::from("file.toml"),
+                duration: 60,
+                start: None,
+                end: None,
+                print_prompt_data: true,
+                dry_run: false,
+            },
+            Config {
+                general: GeneralConfig {
+                    profile: "aws-profile".to_string(),
+                    time_zone: Some("Asia/Manila".to_string()),
+                },
+                open_ai: OpenAiConfig {
+                    api_key: Some("openai-api-key".to_string()),
+                    model: "gpt-4o".to_string(),
+                    max_token: 4096,
+                },
+                app_description: Some(vec![
+                    AppDescConfig {
+                        order_no: 5,
+                        description: "App description".to_string()
+                    },
+                ]),
+                ec2: Some(vec![
+                    Ec2Config {
+                        order_no: 4,
+                        instance_name: "ec2-instance".to_string()
+                    }
+                ]),
+                rds: Some(vec![
+                    RdsConfig {
+                        order_no: 3,
+                        db_identifier: "rds-instance".to_string()
+                    }
+                ]),
+                cloudwatch_metric: Some(vec![
+                    CloudwatchMetricConfig {
+                        order_no: 2,
+                        dimension_name: "dimension-name".to_string(),
+                        dimension_value: "dimension-value".to_string(),
+                        metric_identifier: "metric-identifier".to_string(),
+                        metric_namespace: "metric-namespace".to_string(),
+                        metric_name: "metric-name".to_string(),
+                        metric_stat: "metric-stat".to_string(),
+                    }
+                ]),
+                cloudwatch_log_insight: Some(vec![
+                    CloudwatchLogInsightConfig {
+                        order_no: 1,
+                        description: "description".to_string(),
+                        log_group_name: "log-group-name".to_string(),
+                        query: "query".to_string(),
+                        result_columns: vec![
+                            "col1".to_string(),
+                            "col2".to_string()
+                        ],
+                    }
+                ]),
+            }
+        ).unwrap();
+
+        assert_eq!(context.profile, "aws-profile");
+        assert_eq!(context.time_zone, Tz::Asia__Manila);
+        assert_eq!(context.open_ai_api_key, Some("openai-api-key".to_string()));
+        assert_eq!(context.open_ai_model, "gpt-4o".to_string());
+        assert_eq!(context.open_ai_max_token, 4096);
+        assert_eq!(context.data_sources.len(), 5);
+        assert!(matches!(context.data_sources[0], CloudwatchLogInsight {..}));
+        assert!(matches!(context.data_sources[1], CloudwatchMetric{..}));
+        assert!(matches!(context.data_sources[2], Rds{..}));
+        assert!(matches!(context.data_sources[3], Ec2{..}));
+        assert!(matches!(context.data_sources[4], AppDescription{..}));
+    }
 }
