adbaca0a82bcb909f35ff049c70ae4aeb84e86a7
jedcua <jonedwardcua@gmail.com>
2024-10-13 21:14:11 +0800

    Added more test coverage to ec2

--- src/datasource/ec2.rs
+++ src/datasource/ec2.rs
@@ -2,8 +2,8 @@ use crate::lib::config::Ec2Config;
 use crate::lib::prompt::PromptData;
 use aws_sdk_ec2::operation::describe_instances::DescribeInstancesOutput;
 use aws_sdk_ec2::types::{Filter, Instance};
-use std::error::Error;
 use aws_sdk_ec2::Client;
+use std::error::Error;
 
 pub trait Ec2Client {
     async fn describe_instances(&self, filter: Filter) -> Result<DescribeInstancesOutput, Box<dyn Error>>;
@@ -67,37 +67,65 @@ fn build_description(config: &Ec2Config, instance: Instance) -> Vec<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use aws_sdk_ec2::types::builders::InstanceBuilder;
-    use aws_sdk_ec2::types::{CpuOptions, InstanceState, InstanceStateName, InstanceType};
+    use aws_sdk_ec2::types::{CpuOptions, InstanceState, InstanceStateName, InstanceType, Reservation};
 
-    #[test]
-    fn test_build_description() {
+    struct MockEc2Client { }
+
+    impl Ec2Client for MockEc2Client {
+        async fn describe_instances(&self, _: Filter) -> Result<DescribeInstancesOutput, Box<dyn Error>> {
+            Ok(DescribeInstancesOutput::builder()
+                .reservations(Reservation::builder()
+                    .instances(Instance::builder()
+                        .instance_type(InstanceType::T3aMedium)
+                        .cpu_options(CpuOptions::builder()
+                            .core_count(1)
+                            .threads_per_core(2)
+                            .build())
+                        .state(InstanceState::builder()
+                            .name(InstanceStateName::Running)
+                            .build())
+                        .build())
+                    .build())
+                .build())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_fetch_data() {
+        let client = MockEc2Client {};
         let config = Ec2Config {
             order_no: 1,
             instance_name: "ec2-instance".to_string()
         };
 
-        let instance = InstanceBuilder::default()
-            .instance_type(InstanceType::T3aMedium)
-            .cpu_options(CpuOptions::builder()
-                .core_count(1)
-                .threads_per_core(2)
-                .build()
-            )
-            .state(InstanceState::builder()
-                .name(InstanceStateName::Running)
-                .build()
-            )
-            .build();
+        let prompt_data = fetch_data(client, &config).await.expect("Should be able to fetch data");
 
-        let descriptions = build_description(&config, instance);
-        
-        assert_eq!(descriptions.len(), 6);
-        assert_eq!(descriptions[0], "Information: [EC2 Instance]".to_string());
-        assert_eq!(descriptions[1], "Instance name: [`ec2-instance`]".to_string());
-        assert_eq!(descriptions[2], "Instance type: [`t3a.medium`]".to_string());
-        assert_eq!(descriptions[3], "Cpu core count: [1]".to_string());
-        assert_eq!(descriptions[4], "Cpu threads per core: [2]".to_string());
-        assert_eq!(descriptions[5], "State: [running]".to_string());
+        assert_eq!(prompt_data.description.len(), 6);
+        assert_eq!(prompt_data.description[0], "Information: [EC2 Instance]".to_string());
+        assert_eq!(prompt_data.description[1], "Instance name: [`ec2-instance`]".to_string());
+        assert_eq!(prompt_data.description[2], "Instance type: [`t3a.medium`]".to_string());
+        assert_eq!(prompt_data.description[3], "Cpu core count: [1]".to_string());
+        assert_eq!(prompt_data.description[4], "Cpu threads per core: [2]".to_string());
+        assert_eq!(prompt_data.description[5], "State: [running]".to_string());
+        assert_eq!(prompt_data.data.is_none(), true)
+    }
+
+    struct NoInstanceEc2Client { }
+
+    impl Ec2Client for NoInstanceEc2Client {
+        async fn describe_instances(&self, _: Filter) -> Result<DescribeInstancesOutput, Box<dyn Error>> {
+            Ok(DescribeInstancesOutput::builder()
+                .reservations(Reservation::builder().build())
+                .build())
+        }
+    }
+
+    #[tokio::test]
+    #[should_panic(expected = "Unable to find EC2 instance with name: not-found-instance-name")]
+    async fn test_fetch_instance_not_found() {
+        let client = NoInstanceEc2Client {};
+        let instance_name = String::from("not-found-instance-name");
+
+        fetch_instance(client, &instance_name).await.unwrap();
     }
 }
